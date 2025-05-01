91bdb01532fdce948187fce0d7cc72f8f39a9d15
NOMOREBLACK24 <mno481317@gmail.com>
2025-05-02 03:01:30 +0530

    Update sahil.py

--- sahil.py
+++ sahil.py
@@ -11,12 +11,12 @@ from threading import Thread
 loop = asyncio.get_event_loop()
 
 # Bot token
-TOKEN = '7362289727:AAE6RWfho65aJUxMyOSdvxXVBndKGx4QZcc'
+TOKEN = '7501817760:AAHrKC3d8lRSK8pjp2JoShf6u6EjOAcpl8s'
 bot = telebot.TeleBot(TOKEN)
 REQUEST_INTERVAL = 1
 
 # Admins list
-ADMIN_IDS = [2085082046]  # Replace with actual admin user IDs
+ADMIN_IDS = [6114033856]  # Replace with actual admin user IDs
 
 # File to store user information
 USERS_FILE = 'users.txt'
